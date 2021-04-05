334e9c4e8e489b557158e7693bd0af02533fa9a5
Loki <sneyed@gmail.com>
2021-04-06 04:50:52 +1000

    Use absolute urls for seo-preview.png

--- src/components/header/meta.js
+++ src/components/header/meta.js
@@ -1,4 +1,6 @@
 export default function MetaTags() {
+  const defaultDomain = "https://smashpodium.vercel.app/";
+  const domain = process.env.VERCEL_URL || defaultDomain;
   return (
     <>
       <meta name="title" content="Podium" />
@@ -8,22 +10,22 @@ export default function MetaTags() {
       />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://smashpodium.vercel.app/" />
+      <meta property="og:url" content={domain} />
       <meta property="og:title" content="Podium" />
       <meta
         property="og:description"
         content="Generate results of competitive Smash events"
       />
-      <meta property="og:image" content="/img/seo-preview.png" />
+      <meta property="og:image" content={`${domain}img/seo-preview.png`} />
 
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://smashpodium.vercel.app/" />
+      <meta property="twitter:url" content={domain} />
       <meta property="twitter:title" content="Podium" />
       <meta
         property="twitter:description"
         content="Generate results of competitive Smash events"
       />
-      <meta property="twitter:image" content="/img/seo-preview.png" />
+      <meta property="twitter:image" content={`${domain}img/seo-preview.png`} />
     </>
   );
 }
